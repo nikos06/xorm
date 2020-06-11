4289ec5a51781f4a448f4e94f6cf9beda9df82d0
caoyong <caoyong2619@gmail.com>
2020-06-11 15:58:44 +0800

    add has one relation

---
+++ integrations/session_has_one_test.go
@@ -0,0 +1,50 @@
+package integrations
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHasOne(t *testing.T) {
+	err := PrepareEngine()
+	assert.NoError(t, err)
+
+	type Dog struct {
+		Id       int64
+		MasterId int64
+	}
+
+	type Cat struct {
+		Id       int64
+		MasterId int64
+	}
+
+	type Master struct {
+		Id    int64
+		Name  string
+		Dog *Dog `xorm:"has_one"`
+		Cat *Cat `xorm:"has_one"`
+	}
+
+	err = testEngine.Sync2(new(Master), new(Dog), new(Cat))
+	assert.NoError(t, err)
+
+	//test get
+	master := Master{Id: 1, Name: `master1`}
+	_, err = testEngine.InsertOne(&master)
+	assert.NoError(t, err)
+
+	dog := Dog{Id: 1, MasterId: master.Id}
+	_, err = testEngine.InsertOne(dog)
+	assert.NoError(t, err)
+
+	cat := Cat{Id: 1, MasterId: master.Id}
+	_, err = testEngine.InsertOne(cat)
+	assert.NoError(t, err)
+
+	get := Master{}
+	_, err = testEngine.ID(master.Id).Get(&get)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, get.Dog)
+	assert.NotEmpty(t, get.Cat)
+}
